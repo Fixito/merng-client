659e7b39970eb1eb57c3359761d8847eeb124ec9
Fixito <thomas_3004@hotmail.fr>
2021-10-04 11:18:59 +0200

    init

---
+++ src/ApolloProvider.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import App from './App';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink
+} from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+
+const httpLink = createHttpLink({
+  uri: 'http://localhost:5000'
+});
+
+const authLink = setContext(() => {
+  const token = localStorage.getItem('jwtToken');
+
+  return {
+    headers: {
+      Authorization: token ? `Bearer ${token}` : ''
+    }
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache()
+});
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default () => (
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>
+);
---
+++ src/components/LikeButton.js
@@ -0,0 +1,72 @@
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import { gql, useMutation } from '@apollo/client';
+import { Button } from 'semantic-ui-react';
+
+import MyPopup from '../util/MyPopup';
+
+function LikeButton({ user, post: { id, likes, likeCount } }) {
+  const [liked, setLiked] = useState(false);
+
+  useEffect(() => {
+    if (user) {
+      if (likes) {
+        if (likes.find((like) => like.username === user.username)) {
+          setLiked(true);
+        }
+      }
+    }
+  }, [user, likes]);
+
+  const [likePost] = useMutation(LIKE_POST_MUTATION, {
+    variables: { postId: id }
+  });
+
+  const likeButton = user ? (
+    liked ? (
+      <Button
+        color='teal'
+        icon='heart'
+        label={{
+          basic: true,
+          color: 'teal',
+          pointing: 'left',
+          content: likeCount
+        }}
+        onClick={likePost}
+      />
+    ) : (
+      <Button
+        basic
+        color='teal'
+        icon='heart'
+        label={{
+          basic: true,
+          color: 'teal',
+          pointing: 'left',
+          content: likeCount
+        }}
+        onClick={likePost}
+      />
+    )
+  ) : (
+    <Button icon='heart' color='teal' as={Link} to={'/login'} basic />
+  );
+
+  return <MyPopup content={liked ? 'Unlike' : 'Like'}>{likeButton}</MyPopup>;
+}
+
+const LIKE_POST_MUTATION = gql`
+  mutation likePost($postId: ID!) {
+    likePost(postId: $postId) {
+      id
+      likes {
+        id
+        username
+      }
+      likeCount
+    }
+  }
+`;
+
+export default LikeButton;
